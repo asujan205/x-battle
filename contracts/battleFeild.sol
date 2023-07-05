73b2506994294c9f73bd3f28d8265ccce150e739
asujan205 <asujan205@gmail.com>
2023-07-05 21:35:10 +0545

    #characters

--- contracts/battleFeild.sol
+++ contracts/battleFeild.sol
@@ -10,8 +10,21 @@ import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 contract BattleFeild is ERC1155,ERC1155Supply,Ownable{
 
    string public baseURI;
-   unit256 public totalSupply;
-   
+   uint256 public totalSupply;
+
+   uint256 public constant Ram =5;
+   uint256 public constant shiva =6;
+    uint256 public constant vishnu =7;
+    uint256 public constant brahma =8;
+    uint256 public constant hanuman =9;
+    uint256 public constant ganesha =10;
+    uint256 public constant ravan =0;
+    uint256 public constant kumbhkaran =1;
+    uint256 public constant duryoDhan =2;
+    
+
+
+
 
 
 }
